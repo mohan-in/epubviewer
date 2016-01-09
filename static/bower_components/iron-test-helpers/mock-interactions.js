1fcf1347a2acea8644cb983c103866586d060ab4
gocode <mail2mohanram@gmail.com>
2016-01-09 23:26:05 +0530

    bower update

--- static/bower_components/iron-test-helpers/mock-interactions.js
+++ static/bower_components/iron-test-helpers/mock-interactions.js
@@ -107,14 +107,16 @@
   }
 
   function focus(target) {
-    Polymer.Base.fire.call(target, 'focus', {}, {
-      bubbles: false
+    Polymer.Base.fire('focus', {}, {
+      bubbles: false,
+      node: target
     });
   }
 
   function blur(target) {
-    Polymer.Base.fire.call(target, 'blur', {}, {
-      bubbles: false
+    Polymer.Base.fire('blur', {}, {
+      bubbles: false,
+      node: target
     });
   }
 
@@ -144,7 +146,8 @@
 
   function keyboardEventFor(type, keyCode) {
     var event = new CustomEvent(type, {
-      bubbles: true
+      bubbles: true,
+      cancelable: true
     });
 
     event.keyCode = keyCode;
